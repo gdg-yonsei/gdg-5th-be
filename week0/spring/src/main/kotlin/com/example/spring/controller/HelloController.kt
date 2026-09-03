417961b9c9a627bee24e281754b05ab1209bdd18
jongheon1 <heon0128@gmail.com>
2026-09-03 20:20:41 +0900

    fix: Hello GDGoC!

--- week0/spring/src/main/kotlin/com/example/spring/controller/HelloController.kt
+++ week0/spring/src/main/kotlin/com/example/spring/controller/HelloController.kt
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HelloController {
     @GetMapping("/")
-    fun hello(): String = "Hello Spring!"
+    fun hello(): String = "Hello GDGoC!!"
 }
